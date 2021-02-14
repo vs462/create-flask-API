58faca0e94711ccadc88429893a08275d9d580a6
vs462 <60940617+vs462@users.noreply.github.com>
2021-02-14 16:00:20 +0000

    add postgreSQL

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_sqlalchemy import SQLAlchemy
 from security import authenticate, identity
 import models
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-
-from flask_sqlalchemy import SQLAlchemy
 from db import db
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # to indicate that the db is in the root folder
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # to indicate that the db is in the root folder
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # to turn off Flask modeification tracker to use SQLAlchemy mod. tracker which is better (mod tracker is used to monitor changes of object)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
 
